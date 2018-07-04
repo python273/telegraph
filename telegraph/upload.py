c6d5bcc78f1198886116a8b586b52756a9922c4c
Igor <igorek20045565@gmail.com>
2018-07-04 18:54:49 +0300

    Commit 5

--- telegraph/upload.py
+++ telegraph/upload.py
@@ -4,37 +4,45 @@ import requests
 from .exceptions import TelegraphException
 
 
-def check_file(f):
-    """ :param f: Filename or file-like object.
-    """
-    if hasattr(f, 'read'):
-        if hasattr(f, 'name'):
-            filename = f.name
+def check_file(paths):
+    files = []
+    mimes = []
+    opened_files = []
+    for f in paths:
+        if hasattr(f, 'read'):
+            if hasattr(f, 'name'):
+                filename = f.name
+            else:
+                filename = ''
         else:
-            filename = ''
-        opened = False
-    else:
-        f = open(f, 'rb')
-        filename = f.name
-        opened = True
+            f = open(f, 'rb')
+            filename = f.name
+            opened_files.append(f)
 
-    mime = mimetypes.MimeTypes().guess_type(filename)[0]
-    return f, mime, opened
+        mimes.append(mimetypes.MimeTypes().guess_type(filename)[0])
+        files.append(f)
+    return files, mimes, opened_files
 
 
 def upload_file(f):
-    """ Upload file to Telegra.ph's servers. Returns file link.
+    """ Upload file to Telegra.ph's servers. Returns {"file<number>": "<link>"}.
         Allowed only .jpg, .jpeg, .png, .gif and .mp4 files.
 
         :param f: Filename or file-like object.
+        :type f: file, str or list
     """
-    f, mime, opened = check_file(f)
+    files, mimes, opened_files = check_file(f)
+    files_req = {}
+
+    for x, f in enumerate(files):
+        files_req[f'file{x}'] = (f'file{x}', f, mimes[x])
+
     response = requests.post(
         'http://telegra.ph/upload',
-        files={'file': ('file', f, mime)}
+        files=files_req
     ).json()
 
-    if opened:
+    for f in opened_files:
         f.close()
 
     try:
@@ -44,4 +52,9 @@ def upload_file(f):
     if error:
         raise TelegraphException(error)
 
-    return response[0]['src']
+    res = {}
+    for x, src in enumerate(response):
+        src = f['src']
+        res[f'file{x}'] = (f'{src}')
+
+    return res
